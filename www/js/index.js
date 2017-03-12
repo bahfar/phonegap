d614da1903fa214c1f2ddb7b2bfb2b7bad718276
Hatem Mahmoud <hatem@expressionlab.com>
2017-03-12 23:49:09 +0200

    Updated config and added index.js to open InAppBrowser

---
+++ www/js/index.js
@@ -0,0 +1,27 @@
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+
+        var ref = cordova.InAppBrowser.open('https://shopmate.bahgatfarms.com/categories', '_blank', 'location=no');
+
+        console.log('Received Event: ' + id);
+    }
+};
