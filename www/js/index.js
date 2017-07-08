8da9cf113eeb4b4e5a8569556a3fa42e6c7e8699
Hatem Mahmoud <hatem@expressionlab.com>
2017-07-08 18:36:35 +0200

    Set location and allow navigation for development URL and set version to 2.0.1-alpha

--- www/js/index.js
+++ www/js/index.js
@@ -49,7 +49,8 @@ function onDeviceReady() {
 //  inAppBrowserRef = cordova.InAppBrowser.open("https://shopmate.bahgatfarms.com/categories", "_blank", "location=no,toolbar=no,hidden=yes");
     
 //  window.location.replace("https://shopmate.bahgatfarms.com/categories");
-    window.location.replace("https://storemateapp.com/organizations/bahgatfarms");
+//  window.location.replace("https://storemateapp.com/organizations/bahgatfarms");
+    window.location.replace("https://storemate-hatemmahmoud.c9users.io/organizations/bahgatfarms?platform=app");
 }
 
 function onOffline(){
